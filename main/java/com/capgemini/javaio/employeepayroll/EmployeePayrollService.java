8f39e8a705d5187b6bbe15ccdbfa9a6531e4213e
Mohana Kavya K <kkunisettymohana@student.nitw.ac.in>
2020-10-19 00:22:32 +0530

    Modified : Employee Payroll is Stored n File

--- main/java/com/capgemini/javaio/employeepayroll/EmployeePayrollService.java
+++ main/java/com/capgemini/javaio/employeepayroll/EmployeePayrollService.java
@@ -5,6 +5,9 @@ import java.util.List;
 import java.util.Scanner;
 
 public class EmployeePayrollService {
+	public enum IOService {
+		CONSOLE_IO, FILE_IO, DB_IO, REST_IO
+	}
 		private List<EmployeePayrollData> employeePayrollList;
 
 		public EmployeePayrollService() {
@@ -19,7 +22,7 @@ public class EmployeePayrollService {
 			EmployeePayrollService employeePayrollService = new EmployeePayrollService(employeePayrollList);
 			Scanner consoleInputReader = new Scanner(System.in);
 			employeePayrollService.readEmployeePayrollData(consoleInputReader);
-			employeePayrollService.writeEmployeePayrollData();
+			employeePayrollService.writeEmployeePayrollData(IOService.CONSOLE_IO);
 		}
 		private void readEmployeePayrollData(Scanner consoleInputReader) {
 			System.out.println("Enter the employee ID : ");
@@ -32,8 +35,24 @@ public class EmployeePayrollService {
 			employeePayrollList.add(new EmployeePayrollData(id, name, salary));
 		}
 
-		private void writeEmployeePayrollData() {
-			System.out.println("Writing Employee payroll data on Console: " + employeePayrollList);
+		public void writeEmployeePayrollData(IOService ioService) {
+			if (ioService.equals(IOService.CONSOLE_IO))
+				System.out.println("Writing Employee payroll data on Console: " + employeePayrollList);
+			else if (ioService.equals(IOService.FILE_IO))
+				new EmployeePayrollFileIOService().writeData(employeePayrollList);
+
 		}
+
+		public void printData(IOService ioService) {
+			if(ioService.equals(IOService.FILE_IO))
+				new EmployeePayrollFileIOService().printData();
+		}
+
+		public long countEntries(IOService ioService) {
+			if(ioService.equals(IOService.FILE_IO))
+				return new EmployeePayrollFileIOService().countEntries();
+			return 0;
+		}
+
 
 }
---
+++ test/java/com/capgemini/javaio/employeepayroll/EmployeePayrollServiceTest.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package com.capgemini.javaio.employeepayroll;
+import org.junit.Assert;
+import java.util.Arrays;
+import org.junit.Test;
+
+import com.capgemini.javaio.employeepayroll.EmployeePayrollService.IOService;
+/**
+ * @author Mohana Kavya
+ *
+ */
+public class EmployeePayrollServiceTest {
+	@Test
+	public void gievn3EmployeesShouldMatchEmployeeEntries() {
+		EmployeePayrollData[] arrayOfEmployee = { new EmployeePayrollData(1, "Monica", 100000.0),
+				new EmployeePayrollData(2, "Joey", 200000.0), new EmployeePayrollData(3, "Ross", 300000.0) };
+		EmployeePayrollService employeePayrollService;
+		employeePayrollService = new EmployeePayrollService(Arrays.asList(arrayOfEmployee));
+		employeePayrollService.writeEmployeePayrollData(IOService.FILE_IO);
+		employeePayrollService.printData(IOService.FILE_IO);
+		long entries = employeePayrollService.countEntries(IOService.FILE_IO);
+		System.out.println(entries);
+		Assert.assertEquals(3, entries);
+	}
+}
